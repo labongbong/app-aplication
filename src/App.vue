1fe181b5109b83b0640296a0fc88a7c30c06b92e
labongbong <49775953+labongbong@users.noreply.github.com>
2019-06-26 17:06:49 +0900

    프로젝트 코드 추가

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <todo-header></todo-header>
+    <todo-input v-on:add="addTodoItem"></todo-input>
+    <todo-list v-bind:todolist="todoItems" v-on:remove="removeTodoItem"></todo-list>
+    <todo-footer v-on:clear="removeAllItems"></todo-footer>
+    </div>  
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+  components: {
+    'todo-header': TodoHeader,
+    'todo-input': TodoInput,
+    'todo-list': TodoList,
+    'todo-footer': TodoFooter,
+  },
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    fetchTodoItems: function(){
+      for(var i = 0; i<localStorage.length; i++){
+          var item = localStorage.key(i);
+          this.todoItems.push(item);
+      }
+    },
+
+    addTodoItem: function(value){
+      //배열에 추가
+      this.todoItems.push(value);
+      //저장소에 저장
+      localStorage.setItem(value, value);
+    },
+
+    removeTodoItem: function(todo, index){
+      //배열에서 삭제
+      this.todoItems.splice(index,1);
+      //저장소에서 삭제
+      localStorage.removeItem(todo);
+    },
+
+    removeAllItems: function(){
+      this.todoItems = [];
+      localStorage.clear();
+    }
+  },
+
+    created: function(){ // vue로 instance 생성하고 로딩될때 컴포넌트가 생성되자마자 실행되는 로직
+      this.fetchTodoItems();
+  }
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <transition-group tag="ul" name="todo">
+            <li v-for="(todoItem, index) in todolist" v-bind:key="index">
+                <span>{{ todoItem }}</span>
+                <button v-on:click="removeTodoItem(todoItem, index)">remove</button>    
+            </li>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['todolist'],
+
+    // data() {
+    //     return {
+    //         todoItems: [],
+    //     }
+    // },
+    methods: {
+        // fetchTodoItems: function(){
+        //     for(var i = 0; i<localStorage.length; i++){
+        //         var item = localStorage.key(i);
+        //         this.todoItems.push(item);
+        //     }
+        // },
+        removeTodoItem: function(todo, index){
+            console.log('deleted', todo, index);
+            this.$emit('remove', todo, index);
+            // this.todoItems.splice(index,1);
+            // localStorage.removeItem(todo);
+        },
+    },
+    // created: function(){ // vue로 instance 생성하고 로딩될때 컴포넌트가 생성되자마자 실행되는 로직
+    //     this.fetchTodoItems();
+    // }
+
+};
+
+</script>
+
+<style>
+.todo-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.todo-enter-active, .todo-leave-active {
+  transition: all 1s;
+}
+.todo-enter, .todo-leave-to /* .todo-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
